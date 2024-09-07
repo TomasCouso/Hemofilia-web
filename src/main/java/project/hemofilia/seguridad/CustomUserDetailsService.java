68e8281e057ef24d1a5a74d5a0ce184fe515cd3e
TomasCouso <couso4020@gmail.com>
2024-09-07 16:38:49 -0300

    Configuracion de la seguridad

--- src/main/java/project/hemofilia/seguridad/CustomUserDetailsService.java
+++ src/main/java/project/hemofilia/seguridad/CustomUserDetailsService.java
@@ -1,4 +1,34 @@
 package project.hemofilia.seguridad;
 
-public class CustomUserDetailsService {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import project.hemofilia.entidades.Usuario;
+import project.hemofilia.repositorios.UsuarioRepositorio;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UsuarioRepositorio usuarioRepositorio;
+
+    public CustomUserDetailsService(UsuarioRepositorio usuarioRepositorio) {
+        this.usuarioRepositorio = usuarioRepositorio;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String correo) throws UsernameNotFoundException {
+        Usuario user = usuarioRepositorio.findByCorreo(correo)
+                .orElseThrow(() -> new UsernameNotFoundException("Usuario no encontrado"));
+
+        Set<GrantedAuthority> authorities = new HashSet<>();
+        authorities.add(new SimpleGrantedAuthority(user.getRol().getNombre()));
+
+        return new org.springframework.security.core.userdetails.User(user.getCorreo(), user.getPassword(), authorities);
+    }
 }
--- src/main/java/project/hemofilia/seguridad/SecurityConfig.java
+++ src/main/java/project/hemofilia/seguridad/SecurityConfig.java
@@ -1,4 +1,49 @@
 package project.hemofilia.seguridad;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private final UserDetailsService userDetailsService;
+
+    public SecurityConfig(UserDetailsService userDetailsService) {
+        this.userDetailsService = userDetailsService;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests(authorizeRequests -> authorizeRequests
+                        .requestMatchers("/css/**", "/img/**").permitAll()
+                        .requestMatchers("/login","/error").permitAll()
+                        .requestMatchers("/cliente/**").permitAll()
+                        .requestMatchers("/empleado/**").hasRole("EMPELADO")
+                        .anyRequest().authenticated());
+                /*.formLogin(formLogin -> formLogin
+                        .loginPage("/login")
+                        .usernameParameter("correo")
+                        .defaultSuccessUrl("/default", true)
+                        .permitAll())
+                .logout(logout -> logout
+                        .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                        .logoutSuccessUrl("/login?logout")
+                        .permitAll());*/
+        return http.build();
+    }
 }
