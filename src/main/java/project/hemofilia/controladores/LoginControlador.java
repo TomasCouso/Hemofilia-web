8a1f4517a8eb10ed8ebf9c4b8d61e0dfe827daac
TomasCouso <couso4020@gmail.com>
2025-01-23 12:45:35 -0300

    Se configuro el mensaje de error en la pagina de login

--- src/main/java/project/hemofilia/controladores/LoginControlador.java
+++ src/main/java/project/hemofilia/controladores/LoginControlador.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import project.hemofilia.servicios.UsuarioServicio;
 
 @Controller
@@ -21,10 +22,13 @@ public class LoginControlador {
     }
 
     @GetMapping("/login")
-    public String login(Model model) {
+    public String login(Model model ,@RequestParam(value = "error", required = false) String error) {
         long count = usuarioServicio.contarUsuarios();
         if (count == 0) {
             return "redirect:/registro";
+        }
+        if (error != null) {
+            model.addAttribute("error", "Nombre de usuario o contraseña incorrectos.");
         }
         return "login";
     }
